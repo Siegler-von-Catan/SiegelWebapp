6785f4e730b28d18a02a1ccf30f8880aed6797db
leogeier <leonard.geier@hotmail.com>
2021-01-03 17:12:51 +0100

    allow changing api domain

--- app/util/api.ts
+++ app/util/api.ts
@@ -1,21 +1,23 @@
 // @ts-ignore
 import axios from "axios";
 
+const domain = process.env.DOMAIN || "";
+
 export interface Siegel {
     name: string;
     heightmap: string;
 }
 
 export async function getData() {
-    const result = await axios.get("data");
+    const result = await axios.get(domain + "/data");
     return result.data.siegel as Siegel[];
 }
 
 export async function getDataFor(i: string) {
-    const result = await axios.get("data", {params: {i}});
+    const result = await axios.get(domain + "/data", {params: {i}});
     return result.data.siegel as Siegel;
 }
 
 export function getFileUrl(siegel: string) {
-    return `/siegel?file=${siegel}`
+    return `${domain}/siegel?file=${siegel}`
 }
