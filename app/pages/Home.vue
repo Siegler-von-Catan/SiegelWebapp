94cd45dc1f45815a0d9e6e6d9d44b439330e5892
Lisa Ihde <lisaihde@web.de>
2021-01-19 21:44:57 +0100

    Add horizontal lines, contact area and updated mobile responsive design

--- app/pages/Home.vue
+++ app/pages/Home.vue
@@ -14,27 +14,33 @@
             h1 Erwecke historische Siegel zum Leben!
       .container__row
           .container__col-sm-12.container__col-lg-6
-            #picture
+            .picture
               img(src="../assets/siegelsammlung_grun.jpg" alt="Siegelsammlung Grun")
           .container__col-sm-12.container__col-lg-6
-            #text
+            .text
               h2 Die Siegelsammlung
               p Der Sammler Paul Arnold Grun schenkte der Staats- und Universitätsbibliothek Göttingen seine fast 1500 Abdrücke von Lacksiegeln. Grun hatte schon zu Beginn seiner Militärzeit begonnen, Siegelabdrücke seiner Kameraden zu erbitten und damit eine kleine Siegelsammlung anzulegen. Erweitert wurde die Sammlung später durch weitere Siegelabdrücke, die Mitglieder der Genealogisch-Heraldischen Gesellschaft Göttingens beisteuerten. Im Jahr 2016 wurden die Siegel abfotografiert und im #[a(href="https://sammlungen.uni-goettingen.de/sammlung/slg_1034/", target="_blank") Onlineportal der Universität Göttingen] bereit gestellt. Der Datensatz enthält pro Siegel eine Abbildung, die Maße und den Namen der siegelführenden Familie, wie er unter dem Siegel verzeichnet ist. Darüber hinaus wurden die im Siegel abgebildeten Symbole durch Schlagworte erschlossen.
           .container__col-sm-12.container__col-lg-6
-            #text
+            .text
               h2 Eigene Siegelstempel
               p Mit "FabSeal" kann jede*r in Zukunft wortwörtlich Brief und Siegel geben. Die Webanwendung erweckt die Lacksiegel aus der Sammlung von Paul Arnold Grun wieder zum Leben, indem aus den 2D-Fotos des Siegels ein 3D-Modell automatisch generiert wird. Die 3D-Modelle können anschließend mit dem 3D-Drucker oder Lasercutter erneut physisch hergestellt werden, um ganz einfach zu Hause eigene Siegel zu erschaffen und zu benutzen. Diese Webanwendung kann als Grundlage dienen, um neben Siegeln auch andere Sammlungen wie bspw von Münzen in die 3D-Welt zu bringen.
           .container__col-sm-12.container__col-lg-6
-            #picture
+            .picture
               img(src="../assets/fabricatedSeal.jpg" alt="Selbstgemachter Siegelstempel")
           .container__col-sm-12.container__col-lg-6
-            #picture
+            .picture
               img(src="../assets/pipeline.gif" alt="Pipeline")
           .container__col-sm-12.container__col-lg-6
-            #text
+            .text
               h2 Funktionsweise
-              p Die technische Umsetzung für die Erstellung eines 3D-Modells aus den 2D-Fotos besteht aus verschiedenen Zwischenschritten, die ineinander greifen. Von jedem Lacksiegel der Sammlung existiert ein Foto, aus welchem wir die Siegelabdrücke automatisch freistellen und die Form herausfiltern. Neben rechteckigen und ovalen Siegelformen existieren auch weitere nicht-triviale Formen, die aufwendiger zu erkennen sind. Im nächsten Schritt berechnet unser Algorithmus Höheninformationen für das spätere 3D-Objekt unter Nutzung des Verfahrens "Shape-from-Shading (SfS)". Die Schwierigkeit entstand hierbei vor allem durch die Beleuchtung, die zum Teil ungünstige Schatten wirft, denn unser Programm arbeitet mit nur einem Foto pro Siegel. Das fertige Höhenbild wurde dann mit Bildalgorithmen nachbearbeitet, um das Rauschen zu entfernen bevor es in eine 3D-Modell umgewandelt wird.
-
+              p Die technische Umsetzung für die Erstellung eines 3D-Modells aus den 2D-Fotos besteht aus verschiedenen Zwischenschritten, die ineinander greifen. Von jedem Lacksiegel der Sammlung existiert ein Foto, aus welchem wir die Siegelabdrücke automatisch freistellen und die Form herausfiltern. Neben rechteckigen und ovalen Siegelformen existieren auch weitere nicht-triviale Formen, die aufwendiger zu erkennen sind. Im nächsten Schritt berechnet unser Algorithmus Höheninformationen für das spätere 3D-Objekt unter Nutzung des Verfahrens "Shape-from-Shading (SfS)". Die Schwierigkeit entstand hierbei vor allem durch die Beleuchtung, die zum Teil ungünstige Schatten wirft, denn unser Programm arbeitet mit nur einem Foto pro Siegel. Das fertige Höhenbild wurde dann mit Bildalgorithmen nachbearbeitet, um das Rauschen zu entfernen bevor es in ein 3D-Modell umgewandelt wird.
+          .container__col-sm-12
+            img#line(src="../assets/hr2.svg")
+          .container__col-sm-12
+            #contact
+              a.envelope(href="mailto:fab.seal@gmail.com")
+                span.seal.ss-social-regular
+                  span.embossed FabSeal
 </template>
 
 <script lang="ts">
