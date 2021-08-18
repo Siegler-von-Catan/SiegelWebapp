076981fef3a2c9e23a2581fbb08b23e578c0b14f
Lisa Ihde <lisaihde@web.de>
2021-08-18 13:24:08 +0200

    Start with main page

--- app/pages/Home.vue
+++ app/pages/Home.vue
@@ -20,96 +20,58 @@
   .page
     section(class="container--fluid")
       .container__row
-        #home
-          .container__col-sm-12
-            #siegels
-              Siegel3DCanvas(v-for="(tex, i) in textures" @click="onClick(siegels[i])" :tooltip="siegels[i].name" :heightmap="tex" :offset="i - Math.floor(textures.length / 2)")
-          .container__col-sm-12
-            router-link.home-button#link-browse(to="/browse") Siegel durchstöbern
-          .container__col-sm-12
-            h1 Erwecke historische Siegel zum Leben!
-      .container__row
-        .info-section
-          .text
-            h2 Eigene Siegelstempel
-            p Mit "FabSeal" kann jede*r in Zukunft wortwörtlich Brief und Siegel geben. Die Webanwendung erweckt die Lacksiegel aus der Sammlung von Paul Arnold Grun wieder zum Leben, indem aus den 2D-Fotos des Siegels ein 3D-Modell automatisch generiert wird. Die 3D-Modelle können anschließend mit dem 3D-Drucker oder Lasercutter erneut physisch hergestellt werden, um ganz einfach zu Hause eigene Siegel zu erschaffen und zu benutzen. Diese Webanwendung kann als Grundlage dienen, um neben Siegeln auch andere Sammlungen wie bspw von Münzen in die 3D-Welt zu bringen.
-          .picture
-            picture
-              source(media="(max-width: 421px)" srcset="../assets/fabricatedSeal_281w.webp")
-              source(media="(max-width: 421px)" srcset="../assets/fabricatedSeal_281w.jpg")
-              source(media="(max-width: 843px)" srcset="../assets/fabricatedSeal_562w.webp")
-              source(media="(max-width: 843px)" srcset="../assets/fabricatedSeal_562w.jpg")
-              source(media="(min-width: 844px)" srcset="../assets/fabricatedSeal_1124w.webp")
-              source(media="(min-width: 844px)" srcset="../assets/fabricatedSeal_1124w.jpg")
-              img(src="../assets/fabricatedSeal_1124w.jpg" alt="Selbstgemachter Siegelstempel")
-        .info-section
-          .picture
-            picture
-              source(media="(max-width: 384px)" srcset="../assets/resindruckerErgebnis_256w.webp")
-              source(media="(max-width: 384px)" srcset="../assets/resindruckerErgebnis_256w.jpg")
-              source(media="(max-width: 768px)" srcset="../assets/resindruckerErgebnis_512w.webp")
-              source(media="(max-width: 768px)" srcset="../assets/resindruckerErgebnis_512w.jpg")
-              source(media="(min-width: 769px)" srcset="../assets/resindruckerErgebnis_1024w.webp")
-              source(media="(min-width: 769px)" srcset="../assets/resindruckerErgebnis_1024w.jpg")
-              img(src="../assets/resindruckerErgebnis_1024w.jpg" alt="Resindrucker")
-          .text
-            h2 How to Fabricate
-            p Wie funktioniert nun die Herstellung des Siegels genau? Womit lässt sich ein Siegelstempel herstellen? Beim Durchstöbern der Siegel kann eines ausgewählt werden, welches dann als 3D-Modell angezeigt wird. Dort lässt sich sowohl eine 3D-Vorlage für die und im #[router-link(to="/guide") Herstellung mit dem 3D-Drucker] als auch eine Vektordatei für die Herstellung mit dem Lasercutter runterladen. Die besten Druckergebnisse erzielt ein Resindrucker mittels Kunstharz. Frag am besten im FabLab oder Makerspace in deiner Nähe nach, ob dort ein solcher Drucker vorhanden ist. Alternativ kannst du auch einen herkömmlichen 3D-Drucker zu Hause nutzen, der bspw mit PLA-Filament arbeitet. Hier könnten die Ergebnisse verbessert werden, indem das 3D-Modell ein leicht tieferes Relief erhält. Die Vorlage für den Lasercutter kannst du auch nutzen, um einen Siegelstempel aus Moosgummi mit einem Hobbyplotter herzustellen.
-        .info-section
-          .text
-            h2 Die Siegelsammlung
-            p Der Sammler Paul Arnold Grun schenkte der Staats- und Universitätsbibliothek Göttingen seine fast 1500 Abdrücke von Lacksiegeln. Grun hatte schon zu Beginn seiner Militärzeit begonnen, Siegelabdrücke seiner Kameraden zu erbitten und damit eine kleine Siegelsammlung anzulegen. Erweitert wurde die Sammlung später durch weitere Siegelabdrücke, die Mitglieder der Genealogisch-Heraldischen Gesellschaft Göttingens beisteuerten. Im Jahr 2016 wurden die Siegel abfotografiert und im #[a(href="https://sammlungen.uni-goettingen.de/sammlung/slg_1034/", target="_blank") Onlineportal der Universität Göttingen] bereit gestellt. Der Datensatz enthält pro Siegel eine Abbildung, die Maße und den Namen der siegelführenden Familie, wie er unter dem Siegel verzeichnet ist. Darüber hinaus wurden die im Siegel abgebildeten Symbole durch Schlagworte erschlossen.
-          .picture
-            picture
-              source(media="(max-width: 428px)" srcset="../assets/siegelsammlung_grun_285w.webp")
-              source(media="(max-width: 428px)" srcset="../assets/siegelsammlung_grun_285w.jpg")
-              source(media="(max-width: 856px)" srcset="../assets/siegelsammlung_grun_571w.webp")
-              source(media="(max-width: 856px)" srcset="../assets/siegelsammlung_grun_571w.jpg")
-              source(media="(min-width: 857px)" srcset="../assets/siegelsammlung_grun_1142w.webp")
-              source(media="(min-width: 857px)" srcset="../assets/siegelsammlung_grun_1142w.jpg")
-              img(src="../assets/siegelsammlung_grun_1142w.jpg" alt="Siegelsammlung Grun")
-        .info-section
-          .picture
-            picture
-              source(media="(max-width: 480px)" srcset="../assets/pipeline_320w.webp")
-              source(media="(max-width: 480px)" srcset="../assets/pipeline_320w.gif")
-              source(media="(max-width: 960px)" srcset="../assets/pipeline_640w.webp")
-              source(media="(max-width: 960px)" srcset="../assets/pipeline_640w.gif")
-              source(media="(min-width: 961px)" srcset="../assets/pipeline_1280w.webp")
-              source(media="(min-width: 961px)" srcset="../assets/pipeline_1280w.gif")
-              img(src="../assets/pipeline_1280w.gif" alt="Pipeline")
-          .text
-            h2 Funktionsweise
-            p Die technische Umsetzung für die Erstellung eines 3D-Modells aus den 2D-Fotos besteht aus verschiedenen Zwischenschritten, die ineinander greifen. Von jedem Lacksiegel der Sammlung existiert ein Foto, aus welchem wir die Siegelabdrücke automatisch freistellen und die Form herausfiltern. Neben rechteckigen und ovalen Siegelformen existieren auch weitere nicht-triviale Formen, die aufwendiger zu erkennen sind. Im nächsten Schritt berechnet unser Algorithmus Höheninformationen für das spätere 3D-Objekt unter Nutzung des Verfahrens "Shape-from-Shading (SfS)". Die Schwierigkeit entstand hierbei vor allem durch die Beleuchtung, die zum Teil ungünstige Schatten wirft, denn unser Programm arbeitet mit nur einem Foto pro Siegel. Das fertige Höhenbild wurde dann mit Bildalgorithmen nachbearbeitet, um das Rauschen zu entfernen bevor es in ein 3D-Modell umgewandelt wird.
         .container__col-sm-12
-          img#line(src="../assets/hr2.svg")
+          .teaser
+            video(media="(max-width: 480px)" src="../assets/teaser_vs1.mp4" autoplay loop)
+            h1 Erstelle eigene Siegelstempel 
+              br
+              |oder wähle ein historisches Motiv aus!
+        .links      
+          .container__col-sm-4
+            .group
+              h2 Browse
+              img(src="../assets/siegelsammlung_grun_571w.jpg")
+              p Besuche unsere digitale Ausstellung und erfahre mehr über die enthaltenen Stücke
+              ActionButton(title="Zur Ausstellung" icon="forward" @click="goToBrowse")
+          .container__col-sm-4
+            .group_center
+              h2 Remix
+              img(src="../assets/siegelsammlung_grun_571w.jpg")
+              p Werde kreativ und stelle dein eigenes Siegel zusammen
+              ActionButton(title="Zum Editor" icon="forward" @click="goToRemix")
+          .container__col-sm-4
+            .group
+              h2 Create
+              img(src="../assets/siegelsammlung_grun_571w.jpg")
+              p Lade ein Foto hoch, um es als Siegel ausdrucken zu können
+              ActionButton(title="Zum Upload" icon="forward" @click="goToCreate")
         .container__col-sm-12
-          #contact
-            a.envelope(href="mailto:siegler.von.catan@gmail.com")
-              span.seal
-                span.embossed FabSeal
+          .faq
+            h2 Hier könnte ein FAQ stehen
+
 </template>
 
 <script lang="ts">
 import Component from 'vue-class-component';
-import Siegel3DCanvas from '../components/Siegel3DCanvas.vue';
-import {getFileUrl, getRandomData, openDetails, Siegel} from '../util/api';
 import Vue from 'vue';
 import "../style/home.sass";
+import ActionButton from '../components/ActionButton.vue';
 
-@Component({components: {Siegel3DCanvas}})
-export default class Home extends Vue {
+@Component({components: {ActionButton}})
+export default class Home extends Vue { 
 
-  private textures: string[] = [];
-  private siegels: Siegel[] = [];
-
-  public async mounted() {
-    this.siegels = await getRandomData(3);
-    this.textures = this.siegels.map(s => getFileUrl("heightmap", s));
+  private goToBrowse() {
+   this.$router.push('/browse'); 
   }
 
-  private onClick(s: Siegel) {
-    openDetails(s);
+  private goToRemix() {
+   this.$router.push('/remix'); 
   }
+
+  private goToCreate() {
+   this.$router.push('/create'); 
+  }
+
 }
+
 </script>
